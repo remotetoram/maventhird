0a9a48e481c0dd104fb8a31dc79c40f8331c0f17
remotetoram <remotetoram@gmail.com>
2017-08-27 01:17:11 +0530

    changed to crome

--- src/test/java/com/ram/AppTest.java
+++ src/test/java/com/ram/AppTest.java
@@ -9,13 +9,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class AppTest {
 	@Test
 	public void Login(){
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\Ram\\Desktop\\Ram 5.0\\Softwares\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver","C:\\Users\\Ram\\Desktop\\Ram 5.0\\Softwares\\geckodriver.exe");
 		//DesiredCapabilities capabilities=DesiredCapabilities.firefox();
 		//capabilities.setCapability("marionette", true); 
+		 System.setProperty("webdriver.chrome.driver", ""C:\\Users\\Ram\\Desktop\\Ram 5.0\\Softwares\\chromedriver.exe"");
+                 WebDriver driver = new ChromeDriver();
 		
 		
-		
-		WebDriver driver=new FirefoxDriver();
+		//WebDriver driver=new FirefoxDriver();
 		
 		driver.get("http://www.yahoomail.com");
 		driver.findElement(By.id("login-signin")).click();;
