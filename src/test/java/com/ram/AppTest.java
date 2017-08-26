2a68694e458fc351ec1083526c2bb48dacd0d0f0
remotetoram <remotetoram@gmail.com>
2017-08-27 01:22:05 +0530

    import line added

--- src/test/java/com/ram/AppTest.java
+++ src/test/java/com/ram/AppTest.java
@@ -3,18 +3,19 @@ package com.ram;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class AppTest {
 	@Test
 	public void Login(){
-		//System.setProperty("webdriver.gecko.driver","C:\\Users\\Ram\\Desktop\\Ram 5.0\\Softwares\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Ram\\Desktop\\Ram 5.0\\Softwares\\geckodriver.exe");
 		//DesiredCapabilities capabilities=DesiredCapabilities.firefox();
 		//capabilities.setCapability("marionette", true); 
-		 System.setProperty("webdriver.chrome.driver", ""C:\\Users\\Ram\\Desktop\\Ram 5.0\\Softwares\\chromedriver.exe"");
-                 WebDriver driver = new ChromeDriver();
 		
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ram\\Desktop\\Ram 5.0\\Softwares\\chromedriver.exe");
+         WebDriver driver = new ChromeDriver();
 		
 		//WebDriver driver=new FirefoxDriver();
 		
